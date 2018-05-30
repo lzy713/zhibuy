423136fcee9db5e0d46fce8bc5f0ae8027d6ea72
root <610703669@qq.com>
2018-05-30 10:58:20 +0800

    2018-5-30

--- app/Http/Middleware/LoginMiddleware.php
+++ app/Http/Middleware/LoginMiddleware.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
-
+use Illuminate\Http\Request;
 
 class LoginMiddleware
 {
@@ -18,15 +18,15 @@ class LoginMiddleware
     {
         // return $next($request);//request变量 记录所有的请求参数
 
-          if(session('homeFlag')){
+        if(session('homeFlag')){
 
-                return $next($request);
+            return $next($request);
 
-                } else {
+        } else {
+            
+            return redirect('/login');
 
-                return redirect('/login');
-
-                }
+        }
 
 
     }
